e7c60d1d5483201a1f0a6b063a833f90b0b5bbd8
lukhariz03 <LuqmanHarith333@gmail.com>
2023-08-21 08:56:12 +0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,102 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { Container, Grid, Typography, Paper, TextField, Button, List, ListItem, ListItemText, ListItemSecondaryAction } from '@mui/material';
 
 function App() {
+  const [productName, setProductName] = useState('');
+  const [quantity, setQuantity] = useState('');
+  const [productList, setProductList] = useState([]);
+
+  const handleProductNameChange = (event) => {
+    setProductName(event.target.value);
+  };
+
+  const handleQuantityChange = (event) => {
+    setQuantity(event.target.value);
+  };
+
+  const handleAddProduct = () => {
+    if (productName && quantity) {
+      const newProduct = { name: productName, quantity: parseInt(quantity) };
+      setProductList([...productList, newProduct]);
+      setProductName('');
+      setQuantity('');
+    }
+  };
+
+  const handleIncreaseQuantity = (index) => {
+    const updatedProductList = [...productList];
+    updatedProductList[index].quantity++;
+    setProductList(updatedProductList);
+  };
+
+  const handleDecreaseQuantity = (index) => {
+    const updatedProductList = [...productList];
+    if (updatedProductList[index].quantity > 0) {
+      updatedProductList[index].quantity--;
+    }
+    setProductList(updatedProductList);
+  };
+
+  const handleDeleteProduct = (index) => {
+    const updatedProductList = productList.filter((_, i) => i !== index);
+    setProductList(updatedProductList);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Grid container spacing={3}>
+        <Grid item xs={6}>
+          <Paper elevation={3} style={{ padding: '20px' }}>
+            <Typography variant="h6">Product Page</Typography>
+            <TextField
+              label="Product Name"
+              value={productName}
+              onChange={handleProductNameChange}
+              fullWidth
+              margin="normal"
+            />
+            <TextField
+              label="Quantity"
+              value={quantity}
+              onChange={handleQuantityChange}
+              fullWidth
+              margin="normal"
+            />
+            <Button variant="contained" color="primary" onClick={handleAddProduct}>
+              Add Product
+            </Button>
+          </Paper>
+        </Grid>
+        <Grid item xs={6}>
+          <Paper elevation={3} style={{ padding: '20px' }}>
+            <Typography variant="h6">Product Listing</Typography>
+            <List>
+              {productList.map((product, index) => (
+                <ListItem key={index}>
+                  <ListItemText
+                    primary={product.name}
+                    secondary={`Quantity: ${product.quantity}`}
+                  />
+                  <ListItemSecondaryAction>
+                    <Button onClick={() => handleIncreaseQuantity(index)}>
+                      +
+                    </Button>
+                    <Button onClick={() => handleDecreaseQuantity(index)}>
+                      -
+                    </Button>
+                    {product.quantity === 0 && (
+                      <Button onClick={() => handleDeleteProduct(index)}>
+                        Delete
+                      </Button>
+                    )}
+                  </ListItemSecondaryAction>
+                </ListItem>
+              ))}
+            </List>
+          </Paper>
+        </Grid>
+      </Grid>
+    </Container>
   );
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,48 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react';
+import { render, fireEvent, getByText, getByLabelText, queryByText, getAllByRole } from '@testing-library/react';
+import App from './App'; // Update with the correct path to your App component
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders Product Page and Product Listing titles', () => {
+  const { getByText } = render(<App />);
+  const productPageTitle = getByText('Product Page');
+  const productListingTitle = getByText('Product Listing');
+  expect(productPageTitle).toBeInTheDocument();
+  expect(productListingTitle).toBeInTheDocument();
+});
+
+test('adds a product when "Add Product" button is clicked', () => {
+  const { getByLabelText, getByText, getAllByRole } = render(<App />);
+  const productNameInput = getByLabelText('Product Name');
+  const quantityInput = getByLabelText('Quantity');
+  const addButton = getByText('Add Product');
+  fireEvent.change(productNameInput, { target: { value: 'Test Product' } });
+  fireEvent.change(quantityInput, { target: { value: '5' } });
+  fireEvent.click(addButton);
+  const productNames = getAllByRole('listitem').map(item =>
+    item.querySelector('h3').textContent
+  );
+  expect(productNames).toContain('Test Product');
+});
+
+test('increases and decreases quantity of a product', () => {
+  const { getByText } = render(<App />);
+  const addButton = getByText('Add Product');
+  fireEvent.click(addButton);
+  const increaseButton = getByText('+');
+  const decreaseButton = getByText('-');
+  const quantityText = getByText('Quantity: 0');
+  fireEvent.click(increaseButton);
+  expect(quantityText.textContent).toBe('Quantity: 1');
+  fireEvent.click(decreaseButton);
+  expect(quantityText.textContent).toBe('Quantity: 0');
+});
+
+test('deletes a product when quantity is 0', () => {
+  const { getByText, queryByText, getAllByRole } = render(<App />);
+  const addButton = getByText('Add Product');
+  fireEvent.click(addButton);
+  const deleteButton = getByText('Delete');
+  fireEvent.click(deleteButton);
+  const deletedProduct = queryByText('Test Product');
+  expect(deletedProduct).toBeNull();
 });
